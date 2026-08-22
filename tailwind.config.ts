ce3ad12edbc8cfb77b80b593793feafa8d1eeb0f
Rise Prism <hello@riseprism.com>
2026-08-22 02:48:21 +0000

    Switch heading font to Cormorant Garamond (Andreas alternative)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ export default {
         },
       },
       fontFamily: {
-        heading: ['"Bodoni Moda"', 'serif'],
+        heading: ['"Cormorant Garamond"', 'serif'],
         body: ['Inter', 'sans-serif'],
       },
       fontSize: {
